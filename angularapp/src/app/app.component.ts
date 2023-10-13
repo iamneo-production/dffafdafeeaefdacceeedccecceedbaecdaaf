0f575bbe78e076b958bfd10e985de9f9528943c7
Iamneo <techadmin@iamneo.ai>
2023-10-13 07:08:48 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'IPL-Bidding-FrontEnd';
+  title = 'angularapp';
 }
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,38 +1,16 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AdminComponent } from './admin/admin.component';
-import { OrganizerComponent } from './organizer/organizer.component';
-import { LoginComponent } from './login/login.component';
-import { ErrorComponent } from './error/error.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { TeamComponent } from './team/team.component';
-import { PlayerComponent } from './player/player.component';
-import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AdminComponent,
-    OrganizerComponent,
-    LoginComponent,
-    ErrorComponent,
-    NavbarComponent,
-    TeamComponent,
-    PlayerComponent,
-    RegistrationComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
